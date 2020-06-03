e674d891ee34c85f93768ba3e68331b7643f3c95
BasilioHernandezjahaziel <66385868+BasilioHernandezjahaziel@users.noreply.github.com>
2020-06-03 15:44:30 -0500

    Add files via upload

---
+++ conexionDB.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyecto1;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author jahaziel1999
+ */
+public class conexionDB {
+    
+    private static conexionDB con=null;
+    public static conexionDB getInstance(){
+        if(con==null)
+            con=new conexionDB();
+        return con;
+    }
+    private Connection conn = null;
+    private conexionDB(){
+       String urlDatabase = "jdbc:postgresql://localhost:5432/CRUD";
+        try{
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(urlDatabase, "postgres", "password");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error:" + e.getMessage());
+        }
+        System.out.println("La conexion se realizo sin problemas");
+    }
+}
