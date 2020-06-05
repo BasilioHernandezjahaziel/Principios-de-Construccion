def2dcc7049f9ee2ccb694cf3fb7be961d349c6e
jahaziel1999 <jahaziel1999@jahaziel1999-HP-Notebook>
2020-06-04 20:17:20 -0500

    Conexion a Base de datos

---
+++ ConexionDB/src/conexiondb/ConexionDB.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexiondb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author jahaziel, David, Gabriel
+ */
+public class ConexionDB {
+    
+    private static ConexionDB con=null;
+    public static ConexionDB getInstance(){
+        if(con==null)
+            con=new ConexionDB();
+        return con;
+    }
+    
+    private Connection conn = null;
+    private ConexionDB(){
+       String urlDatabase = "jdbc:postgresql://localhost:5432/CRUD";
+        try{
+            Class.forName("org.postgresql.Driver");
+            conn = DriverManager.getConnection(urlDatabase, "postgres", "password");
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error:" + e.getMessage());
+        }
+        System.out.println("La conexion se realizo sin problemas");
+    } 
+}
---
+++ ConexionDB/src/conexiondb/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package conexiondb;
+
+/**
+ *
+ * @author jahaziel, David, Gabriel
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        ConexionDB con;
+        con=ConexionDB.getInstance();
+        
+    }
+}
